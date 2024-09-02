a7650e6328ea321835ad9a7c73d0a20eab244a14
SC136 <swarchuri06@gmail.com>
2024-09-03 00:49:27 +0530

    first commit

---
+++ repoImages.js
@@ -0,0 +1,13 @@
+const repoImages = {
+  "recipe-book": "https://sc.is-a.dev/recipe-book/images/recipe.jpg",
+  "SC136": "https://i.ibb.co/nfL8F0N/image.png",
+  "WeTall": "https://i.ibb.co/KLQhvqr/repository-open-graph-template.png",
+  "Simple-Music-Bot": "https://raw.githubusercontent.com/SC136/Simple-Music-Bot/main/assests/Simple%20Music%20Bot%20Banner.png",
+  "NNDY": "https://i.ibb.co/z5cX0DR/image.png",
+  "Scord": "https://i.ibb.co/p3qdKPG/mTeojYt.png",
+  "sc-136": "https://i.ibb.co/cT1TsG6/image.png",
+  "personal-website": "https://i.ibb.co/LRFfWyv/image.png",
+  "LUNA": "https://i.ibb.co/XjYgDHM/image.png",
+  "SC136.github.io": "https://i.ibb.co/4MbyrBr/image.png",
+  "Basic-Discord-Bot-Template": "https://i.ibb.co/vmv2BSL/image.png"
+};
---
+++ repoLinks.js
@@ -0,0 +1,12 @@
+const repoLinks = {
+  "SC136.github.io": "https://sc.is-a.dev",
+  "personal-website": "https://sc.is-a.dev/personal-website",
+  "SC136": "https://github.com/SC136",
+  "recipe-book": "https://sc.is-a.dev/recipe-book",
+  "WeTall": "https://sc.is-a.dev/WeTall",
+  "Simple-Music-Bot": "https://top.gg/bot/780838708664467456",
+  "NNDY": "https://sc.is-a.dev/NNDY",
+  "Scord": "https://top.gg/bot/830000867205054484",
+  "sc-136": "https://sc136.js.org",
+  "LUNA": "https://sc.is-a.dev/LUNA",
+};
